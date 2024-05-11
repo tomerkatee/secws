1ab58206b30dab91327f90cc1b33531226885753
tomerkatee <tomerkatee@mail.tau.ac.il>
2024-05-11 18:12:01 +0300

    tokenized and added sklearn moduel

--- hw5/classifier.py
+++ hw5/classifier.py
@@ -1,6 +1,7 @@
 import response_scraper
 import sys
-import re
+import sklearn
+
 
 symbols = [
     '+', '-', '*', '/', '%',   # Arithmetic operators
@@ -25,19 +26,48 @@ MIN_STR_SIZE = 30
 def train(strs: list):
     pass
 
+def tokenize(s: str):
+    res = {}
+    for t in symbols+keywords:
+        res[t] = s.count(t)
+
+    return res
+
+def split_into_programs(c_code_lines):
+
+    programs = []
+    in_includes = True
+    curr = ""
+    for l in c_code_lines:
+        if "#include" in l and not in_includes:
+            programs.append(curr)
+            in_includes = True
+            curr = ""
+        elif in_includes and not l.startswith('#include'):
+            in_includes = False
+
+        curr += l
+    
+    programs.append(curr)
+
+    return programs
+
+
 def main():
     HTTP_regular_responses = response_scraper.scrape_responses(sys.argv[1])
+    # filter by size
     HTTP_regular_responses = [s for s in HTTP_regular_responses if len(s) >= MIN_STR_SIZE]
-    with open("demo.txt", 'r') as file:
-        C_code = file.read()
 
 
-    pattern = r'(#include.*\n)+^(?!#include)'
-    programs = re.split(pattern, C_code, flags=re.MULTILINE)
+    with open("c_code.txt", 'r') as file:
+        c_code_lines = file.readlines()
+    
+    programs = split_into_programs(c_code_lines)
 
-    for idx, program in enumerate(programs[:6], start=1):
-        print(idx)
-        print(program)
+    vectors
+
+
+    
 
 
 if __name__ == "__main__":
--- hw5/use_sctokenizer.py
+++
@@ -1,5 +0,0 @@
-import sctokenizer
-
-tokens = sctokenizer.tokenize_file(filepath='test.cpp', lang='cpp')
-for token in tokens:
-    print(token)
